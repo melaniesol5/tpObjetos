588d3a3741d6c3caabe5518af391acd77e77683c
patriciomn <37555229+patriciomn@users.noreply.github.com>
2018-11-25 12:55:53 -0300

    Update testPiratas.scala

--- testPiratas.scala
+++ testPiratas.scala
@@ -1,16 +1,16 @@
 import org.scalatest.FunSuite
 
 class testPiratas extends FunSuite {
-  val barbaNegra = new Guerrero
-  barbaNegra.poderDePelea = 20
-  barbaNegra.vitalidad = 50
-  val nails = new Cocinero
-  nails.energiaInicial = 100
-  nails.moral = 60
-  nails.listaDeIngredientes = "jamon" :: nails.listaDeIngredientes
-  val albino = new Navegante
-  albino.energiaInicial = 100
-  albino.inteligencia = 50
+  val barbaNegra = new Guerrero(20,50)
+  barbaNegra.energiaInicial_(100)
+  val nails = new Cocinero(60,List("jamon","pimienta"))
+  nails.energiaInicial_(100)
+  val albino = new Navegante(50)
+  albino.energiaInicial_(100)
+  jackSparrow.energiaInicial_(500)
+  jackSparrow.poderDePelea_(200)
+  jackSparrow.inteligencia_(300)
+  jackSparrow.ingredientes_(List("ron"))
 
 
   test("Crear un pirata guerrero y calcular el poder de mando") {
@@ -18,8 +18,8 @@ class testPiratas extends FunSuite {
   }
 
   test("Crear un pirata cocinero y calcular el poder de mando") {
-    assert(nails.poderDeMando() == 60)
-    assert(nails.cantidadDeIngredientes() == 1)
+    assert(nails.poderDeMando() == 120)
+    assert(nails.cantidadDeIngredientes() == 2)
   }
 
   test("Crear un pirata navegante y calcular el poder de mando") {
@@ -27,8 +27,7 @@ class testPiratas extends FunSuite {
   }
 
   test("Calcular el poder de mando de Jack Sparrow") {
-    jackSparrow.energiaInicial = 500
-    assert(jackSparrow.poderDeMando() == 150000)
+    assert(jackSparrow.poderDeMando() == 30000000)
   }
 
   test("Hacer que Jack tome ron con un pirata (guerrero / navegante) y aumente su energia en 100 y disminuya en 50 la energia del pirata acompaniante") {
@@ -38,9 +37,10 @@ class testPiratas extends FunSuite {
   }
 
   test("Hacer que Jack tome ron con un pirata cocinero y aumente su energia en 100 y disminuya en 50 la energia del pirata acompaniante") {
-    jackSparrow.energiaInicial = 500
     jackSparrow.tomarRonCon(nails)
-    assert(jackSparrow.energiaInicial == 600)
+    assert(jackSparrow.energiaInicial == 700)
+    assert(jackSparrow.ingredientes.length == 2)
     assert(nails.energiaInicial == 50)
+    assert(nails.cantidadDeIngredientes() == 1)
   }
 }
